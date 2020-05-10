7e37667d7665192a4c5238e05b3989578f5aafda
Ticia <ticiarodela@gmail.com>
2020-05-09 20:58:03 -0700

    save

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # burger
+
+
+
+
+
+## Technologies Used
+
+Front End- HTML,Javascript,Jquery,Bootstrap
+
+Back End- node.js with npm packages express, handlebars and mysql
+
+
+## Deployed Link
+
+<https://still-basin-69611.herokuapp.com//>
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
